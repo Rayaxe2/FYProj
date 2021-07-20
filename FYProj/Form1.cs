d9d6ffd53e9358f55eff3e22f194ece9f48c73d1
Rayaxe2 <rayaxemailbox@gmail.com>
2021-07-20 09:13:25 +0100

    Found Ways To Rotate and Shrink Lines

--- FYProj/Form1.cs
+++ FYProj/Form1.cs
@@ -20,6 +20,10 @@ namespace FYProj
         GroupBox selectedGroupBox1;
 
         Graphics surface;
+        Pen blackPen = new Pen(Color.Black, 2);
+        Pen bluePen = new Pen(Color.Blue, 2);
+        Pen redPen = new Pen(Color.Red, 2);
+
         List<relationshipPoint> RelPoints = new List<relationshipPoint>();
         Point start, end;
 
@@ -40,11 +44,21 @@ namespace FYProj
         }
 
         //http://csharphelper.com/blog/2015/05/rotate-around-a-point-other-than-the-origin-in-c/
-        Matrix rotateLine(Point anchor, int angle) {
+        private Matrix rotateLine(Point anchor, int angle) {
             Matrix rotationResult = new Matrix();
             rotationResult.RotateAt(angle, anchor);
             return rotationResult;
         }
+
+        private Point shrunkLineStartPoint(Point pointOne, Point pointTwo, double shrinkFactor)
+        {
+            return new Point(
+                Convert.ToInt32(pointOne.X + shrinkFactor * (pointTwo.X - pointOne.X)),
+                Convert.ToInt32(pointOne.Y + shrinkFactor * (pointTwo.Y - pointOne.Y))
+            );
+        }
+        
+        
 
         private void reDrawForm() {
             if (surface != null) {
@@ -56,55 +70,22 @@ namespace FYProj
 
             foreach (relationshipPoint coord in RelPoints) {
                 surface.DrawLine(blackPen, coord.start.X, coord.start.Y, coord.end.X, coord.end.Y);
-                surface.DrawLine(redPen, coord.start.X + 50, coord.start.Y + 50, coord.end.X, coord.end.Y);
-                surface.DrawLine(redPen, coord.start.X - 50, coord.start.Y - 50, coord.end.X, coord.end.Y);
 
-                double angle = Math.Abs(((Math.Atan2((coord.end.X - coord.start.X), (coord.end.Y - coord.start.Y)) * 180 / Math.PI) + 180) - 360);
-                //MessageBox.Show(angle.ToString());
+                Graphics lineSymbol = CreateGraphics();
+                int lineLength = (int)Math.Sqrt((Math.Pow((coord.start.X - coord.end.X), 2) + Math.Pow((coord.start.Y - coord.end.Y), 2)));
+                int shrinkTarget = 130;
+                double shrinkFactor = 1 - ((double) shrinkTarget / (double) lineLength);
+
+                Point anchorPoint = shrunkLineStartPoint(coord.start, coord.end, shrinkFactor);
+                Point shrunkEndSymbolEnds = shrunkLineStartPoint(coord.end, anchorPoint, 0.3);
+
+                lineSymbol.Transform = rotateLine(anchorPoint, 20);
+                lineSymbol.DrawLine(redPen, anchorPoint.X, anchorPoint.Y, shrunkEndSymbolEnds.X, shrunkEndSymbolEnds.Y);
+                lineSymbol.Transform = rotateLine(anchorPoint, 340);
+                lineSymbol.DrawLine(redPen, anchorPoint.X, anchorPoint.Y, shrunkEndSymbolEnds.X, shrunkEndSymbolEnds.Y);
             }
 
-            Graphics foot = CreateGraphics();
-            Pen bluePen = new Pen(Color.Blue, 2);
-            foot.DrawLine(bluePen, 10, 100, 40, 100);
-            foot.DrawLine(bluePen, 10, 100, 40, 80);
-            foot.DrawLine(bluePen, 10, 100, 40, 120);
-
-            double t;
-            int pointAx, pointAy, pointBx, pointBy;
-            t = 0.7;
-            pointAx = 300;
-            pointAy = 50;
-            pointBx = 500;
-            pointBy = 300;
-
-            foot.DrawLine(redPen, pointAx, pointAy, pointBx, pointBy);
-
-            //(Ax+t(Bx−Ax),Ay+t(By−Ay)) - shortens line by the factor t and maintians angle
-            //https://math.stackexchange.com/questions/3058210/how-to-shorten-a-line-but-maintain-its-angle
-            int newX = Convert.ToInt32(pointAx + t * (pointBx - pointAx));
-            int newY = Convert.ToInt32(pointAy + t * (pointBy - pointAy));
-
-            //foot.DrawLine(bluePen, newX, newY, pointBx - 50, pointBy - 50);
-            //foot.DrawLine(bluePen, newX, newY, pointBx + 50, pointBy + 50);
-
-            foot.Transform = rotateLine(new Point(newX, newY), 90);
-            foot.DrawLine(bluePen, newX, newY, pointBx, pointBy);
-        }
-
-        private void reDrawForm(int offsetOne, int offsetTwo)
-        {
-            if (surface != null)
-            {
-                surface.Clear(this.BackColor);
-            }
-            surface = CreateGraphics();
-            Pen blackPen = new Pen(Color.Black, 2);
-
-            foreach (relationshipPoint coord in RelPoints)
-            {
-                surface.DrawLine(blackPen, coord.start.X, coord.start.Y, coord.end.X, coord.end.Y);
-                surface.DrawLine(blackPen, coord.start.X + 50, coord.start.Y + 50, 200, 200);
-            }
+            
         }
 
         private bool isRelEstablished(GroupBox gbOne, GroupBox gbTwo) {
@@ -240,7 +221,8 @@ namespace FYProj
             }
 
             //Creates event handler for drawing lines between 2 group boxes
-            EventHandler drawRelationshipEvent = (s, e) => {
+            EventHandler drawRelationshipEvent = (s, e) =>
+            {
 
             };
             //makes event handler globally referencable and assigns it to the form click event click 
@@ -249,166 +231,7 @@ namespace FYProj
 
 
 
-            /*
-            MouseEventHandler recStart = new MouseEventHandler( //this.MouseDown
-                (s, e) => {
-                    if (startRecorded == false)
-                    {
-                        MessageBox.Show(PointToClient(Cursor.Position).ToString());
-                        begining = this.PointToClient(Cursor.Position);
-                        startRecorded = true;
-                    }
-                }
-            );
-            this.MouseDown += recStart;
-
-            Graphics surface = CreateGraphics();
-            Pen pen1 = new Pen(Color.Black, 2);
-
-            MouseEventHandler drawPreview = new MouseEventHandler( //this.MouseMove
-                (s, e) => {
-                    surface.DrawLine(pen1, begining.X, begining.Y, this.PointToClient(Cursor.Position).X, this.PointToClient(Cursor.Position).Y);
-                    surface = CreateGraphics();
-                }
-            );
-            this.MouseMove += drawPreview;
-
-            MouseEventHandler recEnd = new MouseEventHandler( //this.MouseUp
-               (s, e) => {
-                   if (startRecorded == true)
-                   {
-                       end = this.PointToClient(Cursor.Position);
-                       startRecorded = false;
-                       this.MouseDown -= recStart;
-                       this.MouseMove -= drawPreview;
-                   }
-               }
-           );
-            this.MouseUp += recEnd;
-
-
-            surface.DrawLine(pen1, begining.X, begining.Y, end.X, end.Y);
-            //this.MouseDown -= recStart;
-            //this.MouseUp -= recEnd;
-            //this.MouseMove -= drawPreview;
-            */
         }
 
-
-
-
-
-            //TEST CODE
-        /*
-            Label lbl = new Label();
-            lbl.Name = "Label";
-            lbl.Location = new Point(100, 100);
-            lbl.AutoSize = true;
-            lbl.Text = "Test";
-            this.Controls.Add(lbl);
-
-            GroupBox gb = new GroupBox();
-            gb.Name = "Label";
-            gb.Location = new Point(110, 101);
-            gb.AutoSize = true;
-            gb.Text = "Test";
-            this.Controls.Add(gb);
-
-            int x = Cursor.Position.X;
-            int y = Cursor.Position.Y;
-
-            MessageBox.Show(y.ToString());
-
-
-
-
-
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            //SetupClickEvents(this);
-        }
-
-        private void SetupClickEvents(Control container)
-        {
-            foreach (Control control in container.Controls)
-            {
-                control.Click += HandleClicks;
-            }
-        }
-        private void HandleClicks(object sender, EventArgs e)
-        {
-            Control control = (Control)sender;
-            MessageBox.Show(string.Format("{0} was clicked!", control.Name));
-        }
-
-
-
-
-
-
-
-
-        private void DrawLineEvent(object sender, PaintEventArgs e, int pointOneX, int pointOneY, int PointTwoX, int PointTwoY) {
-            Pen blackPen = new Pen(Color.Black, 3);
-
-            Point point1 = new Point(pointOneX, pointOneY);
-            Point point2 = new Point(PointTwoX, PointTwoY);
-
-            e.Graphics.DrawLine(blackPen, point1, point2);
-        }
-
-
-
-
-
-
-
-        gb.Click += new EventHandler(
-                    (s, e) => {
-                        MessageBox.Show("REEEEEE");
-                    }
-                );
-
-
-
-
-
-
-
-
-        MouseEventHandler dragTableEvent = (s, e) => {
-                    //gb.Location = (Point) Point.Subtract(this.PointToClient(new Point(Cursor.Position.X, Cursor.Position.Y)), (Size) gb.Location);
-                    //gb.Location = Point.Add(this.PointToClient(new Point(Cursor.Position.X, Cursor.Position.Y)), (Size)Point.Subtract(this.PointToClient(new Point(Cursor.Position.X, Cursor.Position.Y)), (Size)gb.Location));
-                    /* gb.Location = Point.Add(
-                        gb.Location, 
-                        (Size) Point.Subtract(
-                            this.PointToClient(new Point(Cursor.Position.X, Cursor.Position.Y)), 
-                            (Size) gb.Location
-                        )
-                    ); 
-                    if (!(this.Bounds.Contains(this.PointToClient(MousePosition)))) { 
-                        gb.Location = this.PointToClient(new Point(Cursor.Position.X - 50, Cursor.Position.Y - 50));
-                    }
-
-                    Point oldLocation = gb.Location;
-                    gb.Location = this.PointToClient(new Point(Cursor.Position.X - (Cursor.Position.X - oldLocation.X), Cursor.Position.Y - (Cursor.Position.Y - oldLocation.Y)));
-                    MessageBox.Show(gb.Location.ToString());
-
-        gb.Location = this.PointToClient(new Point(Cursor.Position.X - 15, Cursor.Position.Y - 10));
-                };
-
-
-
-
-
-
-
-        //TEST CODE
-            //this.Click += new System.EventHandler(this.Form1_Click);
-            //The above added to form1.designer.cs
-            //MessageBox.Show("TEST");
-
-         */
-        }
+    }
 }
